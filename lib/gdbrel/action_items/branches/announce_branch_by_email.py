9fe4528c7120c8a1cd135170343f8a5a698db98b
Joel Brobecker <brobecker@adacore.com>
2014-05-27 13:14:56 -0700

    announce_branch_by_email.py: Also announce .tar.gz archive.
    We are about to switch the bz2 archive to xz, which might be less
    widely available compared to bz2. So also announce the .gz archive,
    so that people not having access to xz tools know immediately that
    the gzip'ed alternative is also available.

--- lib/gdbrel/action_items/branches/announce_branch_by_email.py
+++ lib/gdbrel/action_items/branches/announce_branch_by_email.py
@@ -13,6 +13,8 @@ The prerelease snapshots will be available at:
 
     ftp://%(sourceware.hostname)s/%(sourceware.ftp.branch_public_dir)s/\
 gdb.tar.bz2
+    ftp://%(sourceware.hostname)s/%(sourceware.ftp.branch_public_dir)s/\
+gdb.tar.gz
 
 The sources are also accessible via GIT:
 
