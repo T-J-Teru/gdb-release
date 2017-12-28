d573fd0552acc0c4999fe782efc696233761fb7d
Joel Brobecker <brobecker@adacore.com>
2017-12-28 16:31:31 +0400

    Add a new dump-bugs.py script
    This script dumps the list of fixed bugs for a given release in
    both textual and html format, to help expedite the release process
    when making a minor release.

---
+++ dump-bugs.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+"""List all GDB bugs fixed in a given release.
+
+This script takes a version number as input, and generates a list of
+bugs markes as FIXED with that milestone.  This list can be generated
+either in text format (the default) or in HTML format.
+"""
+
+import argparse
+from os.path import abspath, dirname
+import sys
+
+
+def parse_args(argv=None):
+    """Parse the command-line and return the arguments.
+    """
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--html', action='store_true', default=False,
+                        help=('Dump the list of bugs in HTML format'))
+    parser.add_argument('version',
+                        help=('The GDB release version to look up'))
+    return parser.parse_args(argv)
+
+
+def main(argv=None):
+    """The main subprogram.
+    """
+    args = parse_args(argv)
+
+    ROOT_DIR = dirname(abspath(__file__))
+    LIB_DIR = '%s/%s' % (ROOT_DIR, 'lib')
+    sys.path.insert(0, LIB_DIR)
+
+    from gdbrel.bugzilla import DumpFormat, dump_fixed_bugs
+    from gdbrel.config import Config
+
+    cfg = Config(ROOT_DIR)
+    fmt = DumpFormat.HTML if args.html else DumpFormat.TEXT
+    dump_fixed_bugs(cfg, args.version, fmt)
+
+
+if __name__ == '__main__':
+    main()
---
+++ lib/gdbrel/bugzilla.py
@@ -0,0 +1,58 @@
+import requests
+
+
+class DumpFormat(object):
+    """The various ways to format the information about a bug.
+
+    There are two types of placeholders in those strings:
+        - Those enclosed between curly brackets (eg: '{xxx}')
+          refer to general configuration elements, and get
+          substituted once. This information is usually replaced
+          with information extracted from the config.yaml file.
+        - Those enclosed inside '%(' and ')s' (eg: %(xxx)s').
+          Those refer to elements of each bug returned by
+          bugzilla's REST interface.
+    """
+    TEXT = ' * PR %(component)s/%(id)s (%(summary)s)'
+    HTML = ('<li> <a href="{bugzilla_url}/{show_bug}?id=%(id)s">'
+            'PR %(component)s/%(id)s</a> (%(summary)s)')
+
+
+def get_fixed_bugs(config, version):
+    """Return the list of fixed bugs for the given version.
+
+    PARAMETERS
+        config: a gdbrel.config.Config object.
+        version: The GDB version (a string).
+    """
+    request_info = ('product=gdb',
+                    'resolution=FIXED',
+                    'target_milestone=%s' % version,
+                    'include_fields=%s' % ','.join(('id',
+                                                    'component',
+                                                    'summary')))
+    request_url = '%s/%s/bug?%s' % (config['bugzilla.url'],
+                                    config['bugzilla.rest_suburl'],
+                                    '&'.join(request_info))
+
+    r = requests.get(request_url)
+    return r.json()
+
+
+def dump_fixed_bugs(config, version, fmt):
+    """Dump the list of fixed bugs for the given version.
+
+    PARAMETERS
+        config: a gdbrel.config.Config object.
+        version: The GDB version (a string).
+        fmt: The format to use when printing the description of each bug.
+            Use any of the formats provided by the DumpFormat class above.
+    """
+    data = get_fixed_bugs(config, version)
+    fmt = fmt.format(bugzilla_url=config['bugzilla.url'],
+                     show_bug=config['bugzilla.show_bug_suburl'])
+
+    for bug in data['bugs']:
+        # Remove trailing periods (it looks better).
+        bug['summary'] = bug['summary'].rstrip('.')
+        print(fmt % bug)
