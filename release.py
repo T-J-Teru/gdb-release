fe2d7a6f8256dd05750d0ce2fac176c5026dab2d
Joel Brobecker <brobecker@adacore.com>
2020-09-12 15:09:27 -0700

    release.py: Add handling of the CalledProcessError exception
    This exception can make it all the way to the top level, so handle it
    here, so as to have an opportunity to print an intelligible erorr
    message, rather than crash on a backtrace.

--- release.py
+++ release.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 from os.path import abspath, dirname
+from subprocess import CalledProcessError
 import sys
 
 
@@ -9,13 +10,30 @@ def main():
     LIB_DIR = '%s/%s' % (ROOT_DIR, 'lib')
     sys.path.insert(0, LIB_DIR)
 
+    # This variable gets set if an error gets detected, and is then
+    # expected to contain an error message to be displayed to the user.
+    err_msg = None
+
     import gdbrel
     from gdbrel.errors import FatalError
+    from gdbrel.utils import error
+
     try:
         gdbrel.do_release(ROOT_DIR)
     except FatalError as e:
-        from gdbrel.utils import error
-        error(str(e))
+        err_msg = str(e)
+    except CalledProcessError as e:
+        err_msg = "\n".join([
+            f"The following command returned nonzero ({e.returncode}):",
+            f"$ {e.cmd}",
+        ])
+        if e.output is not None:
+            err_msg += f"\n{e.output}"
+        else:
+            err_msg += "\n[command output sent to stdout just above]"
+
+    if err_msg is not None:
+        error(err_msg)
         sys.exit(1)
 
 
