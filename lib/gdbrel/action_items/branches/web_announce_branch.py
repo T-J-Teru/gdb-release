61778d226868a9575ba35cb70e3e6582f5429fe7
Joel Brobecker <brobecker@adacore.com>
2026-06-12 10:48:12 -0700

    web_announce_branch.py: Adjust and expand instructions
    The instructions have bitrotted a bit, so this commit adjusts them
    as follows:
      - We are using Git now; (this is the main change)
      - Make the note about the gnu.org side more prominent and hopefully
        also clearer.

--- lib/gdbrel/action_items/branches/web_announce_branch.py
+++ lib/gdbrel/action_items/branches/web_announce_branch.py
@@ -4,28 +4,38 @@ from gdbrel.action_items.branches import AbstractBranchCreationAI
 AI_BLURB = """\
 Please post the creation of the branch on the website:
 
-  1. Checkout the ${hl_sbu}htdocs${hl_ebu} repository from\
- sourceware.org:/cvs/gdb
+  1. Clone the ${hl_sbu}htdocs${hl_ebu} repository from\
+ ssh://sourceware.org/git/gdb-htdocs.git
 
   2. cd htdocs
 
   3. Edit news/index.html (use previous annoucements as template)
 
-  4. Copy new entry to index.html
+  4. Copy the new entry to index.html
 
   5. Update last-modified dates:
-     % ./index.sh news/index.html index.html
+
+         % ./index.sh news/index.html index.html
 
   6. Verify the modifications you just made:
-     % cvs diff -up news/index.html index.html | tee announce.diff
+
+         % git diff -up news/index.html index.html
 
   7. Commit the changes:
-     % cvs ci news/index.html index.html
-     (There will be a reminder later for the commit notification to be sent)
 
-     Note: No need worry about www.gnu.org/software/gdb, which is managed
-     via savannah, anymore.  There is a redirect from this website to
-     the one hosted on sourceware.org.
+         % git ci news/index.html index.html
+
+  8. Push the change
+     (this will automatically deploy the change to the GDB website
+     hosted on sourceware.org)
+
+         % git push origin main
+
+
+Note: The GDB website hosted at www.gnu.org/software/gdb is managed via
+      savannah. That website is a simple redirect to the website on
+      sourceware.org, so there is no need to do anything to update
+      the gnu.org side.
 """
 
 
