caec00213ee50c66dacd000297662fdc7fa7d857
Joel Brobecker <brobecker@adacore.com>
2026-06-12 11:09:29 -0700

    unfreeze_branch.py: Provide more detailed instructions

--- lib/gdbrel/action_items/releases/unfreeze_branch.py
+++ lib/gdbrel/action_items/releases/unfreeze_branch.py
@@ -4,8 +4,14 @@ from gdbrel.utils import query
 QUERY_BLURB = """\
 Email the GDB developers that the FREEZE is no longer in effect.
 
-Thank the GDB developers for their patience :-)
+For that, I simply reply to the email that was previously sent
+by this script: It allows mailers to have both the freeze email
+and the unfreeze email to be seen together in people's email.
 
+Hint: You can change the email subject to indicate the unfreeze.
+      For instance:
+
+          gdb-17 branch now *OPEN* (was: "gdb-17 branch FROZEN")
 """
 
 
