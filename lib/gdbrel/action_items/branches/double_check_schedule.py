8d289f322e410ba1cdff6a629244f96c65bffae1
Joel Brobecker <brobecker@adacore.com>
2026-06-12 11:19:54 -0700

    double_check_schedule.py: Provide more detailed info in instructions

--- lib/gdbrel/action_items/branches/double_check_schedule.py
+++ lib/gdbrel/action_items/branches/double_check_schedule.py
@@ -2,12 +2,29 @@ from gdbrel.utils import query
 from gdbrel.action_items.branches import AbstractBranchCreationAI
 
 AI_BLURB = """\
-Please visit the following URL and confirm the schedule:
+Please visit the following URL and check the schedule:
 
     https://www.sourceware.org/gdb/schedule/
 
-If you do make a change, remember to send the patch to gdb-patches.
+If you need to make a change, you can make it via the htdocs repository:
 
+  1. Clone the ${hl_sbu}htdocs${hl_ebu} repository from\
+ ssh://sourceware.org/git/gdb-htdocs.git
+
+  2. cd htdocs
+
+  3. Edit schedule/index.html
+
+  4. Update last-modified date:
+
+       % ./index.sh schedule/index.html
+
+  5. Commit and then push your change
+
+       % git commit schedule/index.html
+       % git push origin main
+
+The deployment to the live GDB website is done automatically.
 """
 
 
