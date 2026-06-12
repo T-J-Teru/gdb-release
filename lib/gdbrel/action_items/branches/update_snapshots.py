46b7cf810a766f8e01b6b674917e3c791a7501c9
Joel Brobecker <brobecker@adacore.com>
2026-06-12 10:40:15 -0700

    update_snapshots.py: Make instructions more explicit

--- lib/gdbrel/action_items/branches/update_snapshots.py
+++ lib/gdbrel/action_items/branches/update_snapshots.py
@@ -7,7 +7,11 @@ Please update the snaphot building process to use the new branch:
      1. Log in as gdbadmin on sourceware.org
      2. cd crontab
      3. Edit crontab and change BRANCH to ${hl_sbu}%(branch_name)s${hl_ebu}
+        You can verify that your change is correct with the following "diff"
+        command:
+          %% cvs diff crontab
      4. Commit change in CVS
+          %% cvs ci crontab
      5. Load the new crontab file:
           %% crontab crontab
 
